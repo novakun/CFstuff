3b7f20dc65edd95d840741d7c7292c2f30daff99
novakun <rafaelsantamarta@gmail.com>
2017-04-28 17:34:49 +0200

    if else cases
    - if player submits contact box without any text we trigger a pop up
    that request them to write something
    - if player submits content in the box we thank them and make
    submission box inactive
    Known BUG: If the user submits multiple times the text displayed on our
    feedback pop up is not being cleared and stacks until page reload.

--- responsive_website/js/scripts.js
+++ responsive_website/js/scripts.js
@@ -28,25 +28,48 @@ $(document).ready(function(){
  });
      //######## TOOLTIPS END #########//
 
+// #############################################
+// ##########################################
      //######## EVENTS ##############//
 $("#submit-tool").on("click", function() {
 	console.log("clicked!");
-	var comment = $(".message-box").val();
-	console.log(comment); /*testing if the msg has been stored in the var*/
-	$("#visible-comment").html(comment);
-	$("#message-box , #name-box , #email-box , #submit-tool").hide(); /*we hide the boxes*/
-	$("#confirm-submition").empty(); /*change submission text label for message sent label*/
-	$("#confirm-submition").append("Message Sent:");
-	$("#confirm-submition , #name-label, #email-label").css("color","#A9A9A9"); /*change colour to inactive for submission box label texts*/
-    $("#popup-msg-sent").modal("toggle"); /*triggers message sent confirmation pop up*/
+
 	
-	return false;
-	// $("#visible-comment").html.(comment);
+/*creating an if else statement to check if the text box was empty when submitting*/
+	// first we store the content of message-box into a var
+	var comment = $(".message-box").val(); 
+	if(comment.length == 0){
+		console.log("text box was empty!");
+		$("#popup-msg-sent").modal("toggle");
+		$("#popup-msg-sent h1").append("Write Something!");
+		$("#popup-msg-sent p").append("You need to write us something for us to be able to help you.");
+		$("#popup-msg-sent").modal("close");
+		$("#popup-msg-sent h1 p").clear();
+		// I WANTED TO KEEP MY HTML CLEAN BY ADDING
+		// ALL THE POPUP CASES IN THE SCRIPT AREA
+		// BUT I'M NOT ABLE TO CLEAR THE CONTENT AFTER 
+		// I FILL IT UP AND THEY STACK UNTIL THE PAGE IS REFRESHED
+	}
+	else{
+		console.log(comment); /*testing if the msg has been stored in the var*/
+		$("#visible-comment").html(comment);
+		$("#message-box , #name-box , #email-box , #submit-tool").hide(); /*we hide the boxes*/
+		$("#confirm-submition").empty(); /*change submission text label for message sent label*/
+		$("#confirm-submition").append("Message Sent:");
+		$("#confirm-submition , #name-label, #email-label").css("color","#A9A9A9"); /*change colour to inactive for submission box label texts*/
+	    $("#popup-msg-sent").modal("toggle"); /*triggers message sent confirmation pop up*/
+	    $("#popup-msg-sent h1").append("Sent!");
+	    $("#popup-msg-sent p").append("Thanks for contacting us!<br/> We'll get back to you as soon as possible!");
+		$("#popup-msg-sent h1 p").clear();
+	};
+
 	
-});
 
+	
+}); /*closing the submit button toggle events */
 
-
+// ############################################
+// ############################################
      //######## END EVENTS ##############//
 
 }); //END OF 'DOC READY FUNCTION'
